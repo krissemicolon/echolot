b27cda8f384557a7ba64615f7b442a20ce92096d
Lian Studer <ln.studer@protonmail.ch>
2022-05-21 00:49:56 +0200

    file content encoding

--- src/transmit.rs
+++ src/transmit.rs
@@ -1,7 +1,12 @@
-pub fn encode() {
-    todo!();
+use std::fs;
+use base64::encode as encode_base64;
+
+pub fn encode() -> anyhow::Result<String> {
+    let content = fs::read_to_string("../demonstration.dat")?;
+    Ok(encode_base64(content))
 }
 
-pub fn transmit() {
-    todo!();
+pub fn transmit() -> anyhow::Result<()> {
+    let content_encoded = encode()?;
+    Ok(())
 }
