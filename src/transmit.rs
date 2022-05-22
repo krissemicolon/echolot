ff79f06d49979a9327c0a0f7b7c4c4d5722737ad
Kris Huber <kris@krishuber.xyz>
2022-05-22 22:59:25 +0200

    Fixed File Path & Sine Demo

--- src/transmit.rs
+++ src/transmit.rs
@@ -4,7 +4,7 @@ use base64::encode as encode_base64;
 mod sound;
 
 pub fn encode() -> anyhow::Result<String> {
-    let content = fs::read_to_string("../demonstration.dat")?;
+    let content = fs::read_to_string("demonstration.dat")?;
     Ok(encode_base64(content))
 }
 
@@ -12,7 +12,7 @@ pub fn transmit() -> anyhow::Result<()> {
     let content_encoded = encode()?;
 
     // To Play Sound:
-    //sound::Sound::new()?.play_freq(freq, len in ms)?;
+    sound::Sound::new()?.play_freq(440.0, 6000)?;
 
     Ok(())
 }
