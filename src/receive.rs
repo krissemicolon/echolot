a6ee9d5560a273aa4e66171ee3106e098262bda9
Kris Huber <kris@krishuber.xyz>
2022-06-20 18:28:44 +0200

    utils

--- src/receive.rs
+++ src/receive.rs
@@ -1,29 +1,66 @@
-use std::fs::File;
+use std::{fs::File, sync::mpsc};
+use std::thread;
+use std::sync::mpsc::{channel, Sender};
 use std::io::prelude::*;
 use base64::decode as decode_base64;
+use cpal::{traits::{HostTrait, DeviceTrait}, Host, SupportedStreamConfig, StreamConfig};
 
 use crate::receive::pitch_detection::gen_demo_samples;
 
+use self::microphone::Microphone;
+
+mod util;
 mod microphone;
 mod pitch_detection;
 
-pub fn decode() -> anyhow::Result<Vec<u8>> {
-    let input = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNldGV0dXIgc2FkaXBzY2luZyBlbGl0ciwKc2VkIGRpYW0gbm9udW15IGVpcm1vZCB0ZW1wb3IgaW52aWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdXlhbSBlcmF0LApzZWQgZGlhbSB2b2x1cHR1YS4=";
-    return Ok(decode_base64(input)?);
+#[derive(Clone, Debug)]
+pub enum MicrophoneEvent {
+    SendData(Vec<f32>),
+    ReceiveData(Sender<Option<Vec<f32>>>),
+}
+
+pub fn decode(samples: Vec<f32>) -> anyhow::Result<Vec<u8>> {
+    // base64 conversion stage
+    // let input = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNldGV0dXIgc2FkaXBzY2luZyBlbGl0ciwKc2VkIGRpYW0gbm9udW15IGVpcm1vZCB0ZW1wb3IgaW52aWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdXlhbSBlcmF0LApzZWQgZGlhbSB2b2x1cHR1YS4=";
+    // return Ok(decode_base64(input)?);
+    Ok(vec![1])
 }
 
 pub fn receive() {
-    let received_content = String::from_utf8(decode().unwrap()).unwrap();
+    let host = cpal::default_host();
+    let device = host.default_input_device().expect("failed to find a default input device");
+    let config = device.default_input_config().unwrap();
+    let sample_rate = config.sample_rate();
 
-    let mut sample_buffer: Vec<f32> = Vec::new();
+    // microphone buffer analyze stage
+    let recording_buffer: Vec<f32> = Vec::new();
+    let (sender, receiver) = mpsc::channel::<MicrophoneEvent>();
 
-    println!("Detected Pitch = {}", pitch_detection::detect_pitch(pitch_detection::gen_demo_samples()));
+    let (channel_count, stream, sampling_rate) = match util::microphone_stream(&host, sender.clone(), &device) {
+        Ok(s) => s,
+        Err(e) => panic!("Error with audio device: {}", e)
+    };
+
+    thread::spawn(move || {
+        util::handle_microphone_events(receiver);
+    });
+
+    // microphone buffer write stage
+
+    // ::: TESTING:
+
+    // println!("Detected Pitch = {}", pitch_detection::detect_pitch(pitch_detection::gen_demo_samples()));
     
+    // microphone rec
     // let mut mic = microphone::Microphone::new(&mut sample_buffer).unwrap();
     // mic.start_listen().unwrap();
     // std::thread::sleep(std::time::Duration::from_millis(10000));
     // mic.stop_listen().unwrap();
 
-    let mut file = File::create("demonstration.dat.rec").unwrap();
-    file.write_all(&received_content.as_bytes()).expect("Couldn't Save Received File");
+    // ::: END TESTING
+
+    // base64 conversion stage
+    // let received_content = String::from_utf8(decode().unwrap()).unwrap();
+    // let mut file = File::create("demonstration.dat.rec").unwrap();
+    // file.write_all(&received_content.as_bytes()).expect("Couldn't Save Received File");
 }
--- src/receive/pitch_detection.rs
+++ src/receive/pitch_detection.rs
@@ -2,7 +2,7 @@ pub fn gen_demo_samples() -> Vec<f32> {
     let mut samples: Vec<f32> = Vec::new();
 
     let mut sample_clock = 0f32;
-    for i in 0..100 {
+    for _ in 0..100 {
         sample_clock = (sample_clock + 1.0) % 44100.0;
         samples.push((sample_clock * 440.0 * 2.0 * std::f32::consts::PI / 44100.0).sin());
     }
